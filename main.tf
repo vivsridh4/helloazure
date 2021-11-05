c81ef888d9b1ce70aba97ff55c3f95d2661e9872
Vivek Sridhar <vivsridh2@gmail.com>
2021-11-05 09:18:01 +0530

    message added

--- main.tf
+++ main.tf
@@ -18,65 +18,32 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "${var.prefix}-k8s"
+  name                = "example-aks1"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "${var.prefix}-k8s"
+  dns_prefix          = "exampleaks1"
 
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_D2_v2"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  addon_profile {
-    aci_connector_linux {
-      enabled = false
-    }
-
-    azure_policy {
-      enabled = false
-    }
-
-    http_application_routing {
-      enabled = false
-    }
-
-    kube_dashboard {
-      enabled = true
-    }
-
-    oms_agent {
-      enabled = false
-    }
+  tags = {
+    Environment = "Production"
   }
-}
-
-output "id" {
-  value = azurerm_kubernetes_cluster.example.id
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
-  sensitive = true
-}
-
-output "client_key" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_key
 }
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
 }
 
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate
-}
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
 
-output "host" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.host
+  sensitive = true
 }
